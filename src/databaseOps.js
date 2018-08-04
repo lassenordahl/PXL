911bf949632100f1aab47cd5e399c67f9cad9633
VasanthSadhasivan <vasanthsadhasivan@gmail.com>
2018-08-04 16:58:43 -0700

    adk

--- src/databaseOps.js
+++ src/databaseOps.js
@@ -2,8 +2,17 @@ var fire = require('./fire');
 
 const database = fire.database();
 
-var starCountRef = database.ref('/').once('value').then(function(snapshot) {
-    snapshot.forEach(function(childSnapshot){
-        console.log(childSnapshot.key);
-    })
-  });
+getEvents();
+
+function getEvents() {
+    events  = []
+    var starCountRef = database.ref('/').once('value').then(function(snapshot) {
+        snapshot.forEach(function(childSnapshot){
+            console.log(childSnapshot.key);
+            console.log(childSnapshot.val()["latne"]);
+            console.log(childSnapshot.val()["lonne"]);
+            console.log(childSnapshot.val()["latsw"]);
+            console.log(childSnapshot.val()["lonsw"]);
+        })
+    });
+}
