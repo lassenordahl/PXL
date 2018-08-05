b7ed770f73e5fd4f11fd81a41f1fc262fe07f9b3
Lasse Nordahl <lasseanordahl@gmail.com>
2018-08-05 02:03:46 -0700

    database rules fix

--- src/components/ColorDisplay.js
+++ src/components/ColorDisplay.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 
+import Button from '@material-ui/core/Button';
+
 class ColorDisplay extends Component {
     
   render() {
     var color = this.props.color;
     return (
-      <div style={{backgroundColor : color, width: "100vw", height: "100vh"}}></div>
+      <div style={{backgroundColor : color, width: "100vw", height: "100vh"}}>
+        {/* <Button variant="fab" mini color="secondary" aria-label="Add">
+        </Button> */}
+      </div>
     )
   }
 }
