9f4466fab714b98e106b0b393c29a9cfb6dc0495
Zachary Zulanas <zzulanas@gmail.com>
2018-08-05 02:33:36 -0700

    help icon added

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import CardSelection from './CardSelection.js'
-import BottomNav from './BottomNav.js';
+import InfoIcon from '@material-ui/icons/Help';
+import { IconButton } from '../../node_modules/@material-ui/core';
+import grey from '@material-ui/core/colors/grey'
 
 const styles = {
   container: {
@@ -13,6 +15,11 @@ const styles = {
   icon: {
     float: 'left',
     width: '32px'
+  },
+  info: {
+    float: 'right',
+    marginTop: '-7.4px',
+    color: grey[900]
   }
 };
 
@@ -24,10 +31,12 @@ class Main extends Component {
           <img style = { styles.icon } src = { require('../assets/PXL.png') } />
           <h2 style = { styles.title }>
             PXL
+            <IconButton style={ styles.info } aria-label="Info">
+              <InfoIcon style={ styles.info }/>
+            </IconButton>
           </h2>
         </div>
         <CardSelection/>  
-        <BottomNav/>
       </div>
     );
   }
