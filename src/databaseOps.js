9ba63f75ed5a3d8d59f34ab600ffc62a4251b6d0
VasanthSadhasivan <vasanthsadhasivan@gmail.com>
2018-08-04 20:14:44 -0700

    asjdhas

--- src/databaseOps.js
+++ src/databaseOps.js
@@ -4,7 +4,6 @@ const database = fire.database();
 
 
 function getEvents() {
-    var events = [];
     return database.ref('/').once('value');
     
     // var starCountRef = database.ref('/').once('value').then(function (snapshot) {
@@ -14,18 +13,28 @@ function getEvents() {
     // });
 }
 
-function changeColor(lat, lon, eventID, color) {
-    var eventsRef = getEvents();
-    eventsRef.then(function(snapshot){
-        console.log("Man titties");
-        for (var i = 0; i < snapshot.val()[eventID]["regions"].length(); i++){
-            console.log(snapshot.val()[eventID]["regions"][i])            
-        }
-        console.log(snapshot.val()[eventID]["regions"]);
-    })    
-}
+function getEvent(lat, lon, eventID) {
+    return database.ref('/'+eventID).once('value');
 
-changeColor(1, 1, "outsideLands", "bLACK");
+    // eventsRef.then(function(snapshot){
+    //     console.log("Man titties");
+    //     for (var i = 0; i < snapshot.val()[eventID]["regions"].length; i++){
+    //         if (snapshot.val()[eventID]["regions"][i]){
+    //             if (lat < snapshot.val()[eventID]["regions"][i]["latne"] &&
+    //                 lat > snapshot.val()[eventID]["regions"][i]["latsw"] &&
+    //                 lon < snapshot.val()[eventID]["regions"][i]["lonne"] &&
+    //                 lon > snapshot.val()[eventID]["regions"][i]["lonsw"]){
+    //                     color = snapshot.val()[eventID]["regions"][i]["color"];
+    //                     break;
+    //                 }
+    //         }            
+    //     }
+    // })    
+}
+var color = "blaxk"
+changeColor(1, 1, "outsideLands", color);
+
+console.log(color)
 
 module.exports = {
     getEvents: getEvents
