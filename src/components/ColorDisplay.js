7b91198ae6f764f9fe18b4c3677e848e0baf3e93
Zachary Zulanas <zzulanas@gmail.com>
2018-08-05 06:09:53 -0700

    ???

--- src/components/ColorDisplay.js
+++ src/components/ColorDisplay.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
+
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import ErrorIcon from '@material-ui/icons/Error';
+import Error from '@material-ui/icons/Error';
 
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
